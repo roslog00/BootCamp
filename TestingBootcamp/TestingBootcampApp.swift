ac40d900912c8ff5c6e205672b0d4ba5c7346ee5
Chebakov <nomwor-dukryk-9Hybpy>
2024-05-17 16:28:57 +0300

    Initial Commit

---
+++ TestingBootcamp/TestingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingBootcampApp.swift
+//  TestingBootcamp
+//
+//  Created by Георгий Чебаков on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
